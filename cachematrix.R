6dd18d070aa5928288152e2a6fb780a4a4b4c7ec
Victor_Alanis <victor_alanis@live.com>
2020-06-25 21:43:12 -0500

    Final product

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions make an special object that is a matrix but it's inside
+## the cache of R and solve the matrix also inside of the R cache
 
-## Write a short comment describing this function
+
+## This function make an objet that's a matrix inside the r cache
+## and return it as a list with several "methods" kinda a OOP view
+
 
 makeCacheMatrix <- function(x = matrix()) {
-
+      m<- NULL
+      set<- function(y){
+        x <<-y
+        m <<-NULL
+      }
+      get <- function() x
+      setinverse<-function(solve) m<<-solve
+      getinverse<-function() m
+      list(set = set, get = get,
+           setinverse = setinverse,
+           getinverse = getinverse)
+      
 }
 
 
-## Write a short comment describing this function
+## this function read the methods of the special object and use them to 
+## calculate the inverse of the matrix and store it in the cache
+## if it finds that de inverse is already in the cache it prints it out 
+## and don't calculate it.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data,...)
+  x$setinverse(m)
+  m
 }
+
+
+##little example
+a=matrix(c(1,2,3,4),nrow=2,ncol=2)
+z=makeCacheMatrix(a)
+cacheSolve(z)
